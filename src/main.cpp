b25f31cecf0759212cc382f9d718365165958744
AvneeshP30 <avneesh.pandey_cs.aiml24@gla.ac.in>
2026-02-20 11:31:03 +0530

    initialize Serial communication and configure IR pin in setup

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,10 @@ IR_PIN 5
 
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
+    pinMode(IR_PIN, INPUT);
+    Serial.begin(9600);
 }
+
 
 void loop() {
 
